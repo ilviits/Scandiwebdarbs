017c9e5eb84a7cd8c97b7b035657027fe239c10b
ilviits <125364303+ilviits@users.noreply.github.com>
2023-02-14 14:12:52 +0200

    Add files via upload

---
+++ addproduct/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css" />
+  <title>Product Add</title>
+</head>
+<body>
+  <div style="text-align: center; margin-bottom: 20px;">
+    <form action='../' method="POST">
+            <input type="submit" style="margin-left: 55%; margin-top: -6.5%" value="Cancel" name = "checkbox"  id = "checkbox"/>
+            <button>Save</button>
+        </form>
+  </div>
+  <h1 style="display: inline-block;">Product Add</h1>
+  <hr>
+  <div>
+  <div class="footer">
+    Scandiweb Test Assignment
+  </div>
+</body>
+</html>
